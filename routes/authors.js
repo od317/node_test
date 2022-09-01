6a639b5394742820bda163c586afd126333d2f40
osama drbas <osamadrbas31@gmail.com>
2022-09-01 23:53:23 +0300

    authors serach

---
+++ routes/authors.js
@@ -0,0 +1,54 @@
+const express =require('express');
+const router=express.Router();
+const mysql=require('mysql');
+
+
+const con= mysql.createConnection({
+    host:"sql6.freesqldatabase.com",
+    user:"sql6516510",
+    password:"HpQe9ARfGn",
+    database:"sql6516510"
+});
+
+
+
+
+//all authors route
+router.get('/',(req,res)=>{
+    let a;  
+    if(req.query.name!=null && req.query.name!==' ')
+      {
+        con.query(`select * from authors where name like "%${req.query.name}%";`,(err,ress,file)=>{
+            a=ress;
+            res.render("authors/index",{authors:a});
+          }) 
+           
+      }
+    else{
+    con.query("select * from authors;",(err,ress,file)=>{
+     a=ress;
+     res.render("authors/index",{authors:a});
+   }) 
+}
+})
+
+// new author route
+router.get('/new',(req,res)=>{
+    res.render('authors/new');
+})
+//creater author
+router.post('/',(req,res)=>{
+    let s=String(req.body.name);
+    con.query(`insert into authors(name) values("${s}");`,(err,ress,file)=>{
+       if(err){
+          res.render('authors/new',{
+            errormas:"error you idot"
+          })
+       }
+       else{
+        res.redirect('authors/new');
+       }
+    })
+});
+
+module.exports=router;
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app=express();
 const expresslay=require('express-ejs-layouts');
 const mysql=require("mysql");
 const indexr=require('./routes/index')
-
+const authrouter=require('./routes/authors')
+const bodyparser=require('body-parser');
 
 const con= mysql.createConnection({
     host:"sql6.freesqldatabase.com",
@@ -12,16 +13,13 @@ const con= mysql.createConnection({
     database:"sql6516510"
 });
 
-con.query("select * from users",(err,res,fil)=>{
-    console.log(res);
-})
-
 app.set(`view engine`,'ejs');
 app.set('views',__dirname+'/views');
 app.set('layout','layouts/layout');
 app.use(expresslay)
 app.use(express.static('public'))
-
+app.use(bodyparser.urlencoded({limit:"10mb",extended:false}));
 app.use('/',indexr);
+app.use('/authors',authrouter);
 
 app.listen(process.env.PORT||3000);
